3a7afa839664f90a264124cb2c2bc0e03b085188
Olaife Olawore <olaifeolawore@gmail.com>
2022-04-19 14:43:51 -0700

    task 0 -take 3 modification

--- main.h
+++ main.h
@@ -24,5 +24,8 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *s, int index);
 int print_buf(char *buf, unsigned int nbuf);
+int print_chr(va_list arguments, char *buf, unsigned int ibuf);
+int print_str(va_list arguments, char *buf, unsigned int ibuf);
+int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 
 #endif
